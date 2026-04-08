ebb231539fb48c14ff549c86b377f81a3e134995
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-08 21:54:52 +0300

    Added Scalar api reference

--- spydle-api/Program.cs
+++ spydle-api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using spydle_api;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,12 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
