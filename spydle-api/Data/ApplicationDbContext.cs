209f31a877c97ef097a3db5dce24e23d97c1a016
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-11 18:54:19 +0300

    CharacterEyeColor and CharacterSkinColor tables have been created with seed data

--- spydle-api/Data/ApplicationDbContext.cs
+++ spydle-api/Data/ApplicationDbContext.cs
@@ -14,5 +14,29 @@ namespace spydle_api.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CharacterEyeColor>(b =>
+            {
+                b.HasData(
+                    new CharacterEyeColor { Id = 0, Name = "Brown", Red = 97, Green = 58, Blue = 30 },
+                    new CharacterEyeColor { Id = 1, Name = "Blue", Red = 46, Green = 87, Blue = 209 },
+                    new CharacterEyeColor { Id = 2, Name = "Green", Red = 125, Green = 212, Blue = 59 },
+                    new CharacterEyeColor { Id = 3, Name = "Grey", Red = 155, Green = 166, Blue = 199 }
+                );
+            });
+
+            modelBuilder.Entity<CharacterSkinColor>(b =>
+            {
+                b.HasData(
+                    new CharacterSkinColor { Id = 0, Name = "Cyan", Red = 41, Green = 214, Blue = 214 },
+                    new CharacterSkinColor { Id = 1, Name = "Yellow", Red = 255, Green = 217, Blue = 15 },
+                    new CharacterSkinColor { Id = 2, Name = "Red", Red = 214, Green = 41, Blue = 73 },
+                    new CharacterSkinColor { Id = 3, Name = "Green", Red = 52, Green = 214, Blue = 41 }
+                );
+            });
+        }
     }
 }
--- spydle-api/Models/CharacterEyeColor.cs
+++ spydle-api/Models/CharacterEyeColor.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace spydle_api.Models
 {
     public class CharacterEyeColor
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public byte Red { get; set; }
