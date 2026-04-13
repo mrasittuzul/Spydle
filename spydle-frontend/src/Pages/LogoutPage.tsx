a8fd60db72c22f684860a20b2408da2734e668ba
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-13 21:34:28 +0300

    Implemented auto navigation to home after logging out

--- spydle-frontend/src/Pages/LogoutPage.tsx
+++ spydle-frontend/src/Pages/LogoutPage.tsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function LogoutPage(){
+    localStorage.removeItem("jwt");
+    const navigate = useNavigate();
+    useEffect(() => {
+        var timer = setTimeout(() => navigate("/"), 1000);
+        return () => {
+            clearTimeout(timer);
+        };
+    })
+
     return(
         <>
             <h1>You have been logged out!</h1>
