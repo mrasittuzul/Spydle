0a838bedd57efd08e3dd4067ca95554f87de728e
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-13 21:22:17 +0300

    Login and Logout components are now rendered conditionally

--- spydle-frontend/src/Components/Header.tsx
+++ spydle-frontend/src/Components/Header.tsx
@@ -1,11 +1,16 @@
 import { NavLink } from "react-router-dom";
 
 export default function Header(){
+
+    const jwt = localStorage.getItem('jwt');
+
     return(
         <nav>
             <NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink>
-            <NavLink to="/Login" className={({ isActive }) => isActive ? "active" : ""}>Log In</NavLink>
-            <NavLink to="/Logout" className={({ isActive }) => isActive ? "active" : ""}>Log Out</NavLink>
+            {
+                jwt ? <NavLink to="/Logout" className={({ isActive }) => isActive ? "active" : ""}>Log Out</NavLink>
+                    : <NavLink to="/Login" className={({ isActive }) => isActive ? "active" : ""}>Log In</NavLink>
+            }
         </nav>
     );
 }
