a3a52947caa9870e37669d62e4e8f6ccd86332cc
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-16 23:58:31 +0300

    Implemented login for LoginPage

--- spydle-frontend/src/Pages/LoginPage.tsx
+++ spydle-frontend/src/Pages/LoginPage.tsx
@@ -1,19 +1,49 @@
+import { useRef, useState } from "react";
+import { Post } from "../Helpers/RequestHelper";
+import type { ApiLoginResponse, ApiErrorResponse } from "../Helpers/RequestHelper";
+
 export default function LoginPage(){
+
+    const [loginErrors, setLoginErrors] = useState<string[]>();
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    console.log(loginErrors);
+    async function onLoginButtonClicked(){
+        if (!emailRef.current || !passwordRef.current){
+            console.error("Email or Password input field reference is null");
+            return;
+        };
+
+        const email: string = emailRef.current.value;
+        const password: string = passwordRef.current.value;
+        const response = await Post("Account/Login", { Email: email, Password: password });
+        if(response.status == 200){
+            const loginResponse = response.data as ApiLoginResponse;
+            localStorage.setItem("jwt", loginResponse.token);
+            localStorage.setItem("jwtExpireDate", loginResponse.tokenExpireDate);
+
+        } else{
+            setLoginErrors((response.data as ApiErrorResponse).errors)
+        }
+    }
+
     return(
         <div>
             <ul>
                 <li>
-                    <label>Username</label>
-                    <input></input>
+                    <label>Email</label>
+                    <input ref={emailRef}></input>
                 </li>
                 <li>
                     <label>Password</label>
-                    <input type="password"></input>
+                    <input ref={passwordRef} type="password"></input>
                 </li>
                 <li>
-                    <button>Login</button>
+                    <button onClick={onLoginButtonClicked}>Login</button>
                 </li>
             </ul>
+            {loginErrors && <ul>{loginErrors.map((err, index) => <li key={index}>{err}</li>)}</ul>}
         </div>
     );
 }
