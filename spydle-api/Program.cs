5197af9ad1c5aee74290ed7758d0b08834e875a6
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-16 18:18:46 +0300

    Added cors policy to the backend

--- spydle-api/Program.cs
+++ spydle-api/Program.cs
@@ -7,7 +7,6 @@ using spydle_api.Data;
 using spydle_api.Interfaces;
 using spydle_api.Models;
 using spydle_api.Services;
-using System.Reflection.Metadata;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,6 +37,15 @@ builder.Services.AddSwaggerGen(setup =>
     setup.AddSecurityRequirement((doc) => new OpenApiSecurityRequirement
     {
         [new OpenApiSecuritySchemeReference("Bearer", doc)] = new List<string>()
+    });
+});
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
     });
 });
 
@@ -89,10 +97,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(MyAllowSpecificOrigins);
+
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapSwagger().RequireAuthorization();
 
 app.Run();
