a80ee3ce7ae7e5ee59c995d4af2a3107d2282955
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-23 20:33:22 +0300

    Character class now extents Phaser.GameObjects.Container

--- spydle-frontend/src/Game/Classes/Character.tsx
+++ spydle-frontend/src/Game/Classes/Character.tsx
@@ -1,11 +1,14 @@
 import * as Phaser from "phaser"
 import type GameplayScene from "../GameScenes/GameplayScene";
 
-export class Character {
+export class Character extends Phaser.GameObjects.Container {
     readonly scene: GameplayScene
     
     code: number;
     characterTraits: CharacterTraits;
+
+    isOutlineVisible: boolean = false;
+    outlineColor: Phaser.Display.Color = new Phaser.Display.Color(0, 0, 0);
 
     baseImage!: Phaser.GameObjects.Image;
     eyeSockets!: Phaser.GameObjects.Image;
@@ -14,19 +17,16 @@ export class Character {
     outline!: Phaser.GameObjects.Image;
     container!: Phaser.GameObjects.Container;
 
-    posX: number;
-    posY: number;
-    constructor(scene: GameplayScene, code: number, posX: number, posY: number) {
+    constructor(scene: GameplayScene, code: number, x: number, y: number) {
+        super(scene, x, y);
         this.scene = scene;
         this.code = code;
-        this.posX = posX;
-        this.posY = posY;
-        this.characterTraits = this.DecodeCharacterTraitsFromCode(code);
-        this.CreateContainer();
-        this.SetCharacterTraits(this.characterTraits);
+        this.characterTraits = this.decodeCharacterTraitsFromCode(code);
+        this.createContainer();
+        this.setCharacterTraits(this.characterTraits);
     }
 
-    DecodeCharacterTraitsFromCode(code: number) : CharacterTraits {
+    decodeCharacterTraitsFromCode(code: number) : CharacterTraits {
         return {
             Sex:            (code & TraitMasks.Sex)            >> 0,
             Age:            (code & TraitMasks.Age)            >> 1,
@@ -35,30 +35,36 @@ export class Character {
         };
     }
 
-    CreateContainer() : void {
+    createContainer() : void {
         this.baseImage = this.scene.add.image(0, 0, "").setOrigin(0, 0);
         this.eyeSockets = this.scene.add.image(0, 0, "eyeSockets").setOrigin(0, 0);
         this.eyeFill = this.scene.add.image(0, 0, "eyeFill").setOrigin(0, 0);
         this.oldOverlay = this.scene.add.image(0, 0, this.characterTraits.Sex === 0 ? "maleOldOverlay" : "femaleOldOverlay").setOrigin(0, 0).setVisible(this.characterTraits.Age === 1);
         this.outline = this.scene.add.image(0, 0, "").setOrigin(0, 0).setVisible(false);
 
-        this.container = this.scene.add.container(this.posX, this.posY, [ this.baseImage, this.eyeSockets, this.eyeFill, this.oldOverlay, this.outline ]);
+        this.add([this.baseImage, this.eyeSockets, this.eyeFill, this.oldOverlay, this.outline]);
+        this.scene.add.existing(this);
     }
 
-    SetCharacterTraits(characterTraits: CharacterTraits){
+    setCharacterTraits(characterTraits: CharacterTraits){
         const baseTexture = characterTraits.Sex === 0 ? "maleBase" : "femaleBase";
         const outlineTexture = characterTraits.Sex === 0 ? "maleOutline" : "femaleOutline";
         const skinColor = this.scene.skinColors[characterTraits.EyeColorIndex]; 
         const eyeColor = this.scene.eyeColors[characterTraits.EyeColorIndex];
-        const isOld = characterTraits.Age === 1;
         const oldOverlayTexture = characterTraits.Age === 0 ? "maleOldOverlay" : "femaleOldOverlay";
 
         this.baseImage.setTexture(baseTexture);
         this.outline.setTexture(outlineTexture);
         this.baseImage.setTint(new Phaser.Display.Color(skinColor.red, skinColor.green, skinColor.blue).color);
         this.eyeFill.setTint(new Phaser.Display.Color(eyeColor.red, eyeColor.green, eyeColor.blue).color);
-        this.oldOverlay.setVisible(isOld);
         this.oldOverlay.setTexture(oldOverlayTexture);
+    }
+
+    setOutline(isVisible: boolean, color: Phaser.Display.Color){
+        this.isOutlineVisible = isVisible;
+        this.outlineColor = color;
+        this.outline.setVisible(isVisible);
+        this.outline.setTint(color.color);
     }
 }
 
