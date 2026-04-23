8728609b9514c8ecec807968d2031b7e8550dd6b
Raşit Tüzül <rasittuzul@gmail.com>
2026-04-23 20:41:08 +0300

    Finished setting up the GameplayScene layout

--- spydle-frontend/src/Game/GameScenes/GameplayScene.tsx
+++ spydle-frontend/src/Game/GameScenes/GameplayScene.tsx
@@ -10,12 +10,22 @@ import femaleBase from '../../assets/GameAssets/FemaleBase.png';
 import femaleOutline from '../../assets/GameAssets/FemaleOutline.png';
 import maleBase from '../../assets/GameAssets/MaleBase.png';
 import maleOutline from '../../assets/GameAssets/MaleOutline.png';
+import white from '../../assets/GameAssets/white.png';
+import { SuspectGroup } from "../Classes/SuspectGroup";
 
 export default class GameplayScene extends Phaser.Scene {
     caseData: {rngSeed: number, includedCharacters: number[]} = {rngSeed:0, includedCharacters:[]};
     eyeColors: {red: number, green: number, blue: number}[] = [];
     skinColors: {red: number, green: number, blue: number}[] = [];
     characters: Character[] = [];
+    suspectGroups: SuspectGroup[] = [];
+
+    submitButton!: Phaser.GameObjects.Image;
+    submitButtonText!: Phaser.GameObjects.Text;
+    submitButtonActiveColor: number = 0x1e93ee;
+    submitButtonInactiveColor: number = 0x104065;
+
+    statusText!: Phaser.GameObjects.Text;
 
     constructor() {
         super({ key: "GameplayScene" });
@@ -37,10 +47,28 @@ export default class GameplayScene extends Phaser.Scene {
         this.load.image('femaleOutline', femaleOutline);
         this.load.image('maleBase', maleBase);
         this.load.image('maleOutline', maleOutline);
+        this.load.image('white', white);
     }
 
     create() {
+        // Character area separator
+        const separator  = this.add.graphics();
+        separator.fillStyle(0xffffff);
+        separator.fillRect(350, 0, 5, this.game.config.height as number);
+
+        this.submitButton = this.add.image(100, 613, "white").setDisplaySize(150, 64).setTint(this.submitButtonActiveColor).setOrigin(0, 0);
+        this.submitButtonText = this.add.text(175, 613 + 64/2, "Submit", {
+            fontSize: "18px",
+            color: "#000000"
+        }).setOrigin(0.5);
+
+        this.statusText = this.add.text(175, 560, "Status", {
+            fontSize: "18px",
+            color: "#ffffff"
+        }).setOrigin(0.5).setVisible(false);
+
         this.spawnCharacters();
+        this.spawnSuspectGroups();
     }
 
     spawnCharacters() : void {
@@ -55,6 +83,19 @@ export default class GameplayScene extends Phaser.Scene {
             const posX = startingPosition.x + (i % columnCount) * (textureSize.width + columnGap);
             const posY = startingPosition.y + Math.floor(i / columnCount) * (textureSize.height + rowGap)
             this.characters.push(new Character(this, this.caseData.includedCharacters[i], posX, posY));
+        }
+    }
+
+    spawnSuspectGroups() : void {
+        const suspectGroupGap = 84;
+        const startingPos = { x: 25, y: 25 };
+        const suspectGroupCount = 6;
+        const suspectCountPerGroup = 5;
+        const backgroundColor = new Phaser.Display.Color(44, 44, 44);
+
+        for(let i = 0; i < suspectGroupCount; i++){
+            const suspectGroup = new SuspectGroup(this, suspectCountPerGroup, backgroundColor, startingPos.x, startingPos.y + suspectGroupGap * i);
+            this.suspectGroups.push(suspectGroup);
         }
     }
 }
